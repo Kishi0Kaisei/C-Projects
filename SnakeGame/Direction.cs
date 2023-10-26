0a8bcac39cdec680a05c4fedd7e7aea1a4aee2e6
Kishi0Kaisei <132790237+Kishi0Kaisei@users.noreply.github.com>
2023-10-26 17:14:05 +0100

    Create SnakeGame
    Creating a snake game sample

---
+++ SnakeGame/Direction.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeGame
+{
+   public class Direction
+    {
+       private int RowOffset { get; }
+       private int ColOffset { get; }
+
+        public Direction(int rowOffset, int colOffset)
+        {
+            RowOffset = rowOffset;
+            ColOffset = colOffset;
+        }
+    }
+}
